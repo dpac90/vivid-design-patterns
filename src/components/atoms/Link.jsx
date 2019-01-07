665fd10a8afde92e4a2fb32279af79789e13bacd
Paige Vogenthaler <paige.vogenthaler@vividseats.com>
2019-01-07 11:13:52 -0600

    WEB-5692 add link component and typography scss

---
+++ src/components/atoms/Link.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+const Link = ({ children, className, href, size }) => {
+    Link.propTypes = {
+        children: PropTyles.node,
+        className: PropTypes.string,
+        href: PropTypes.string.isRequired,
+        size: PropTypes.oneOf(['body-1', 'body-2', 'small', 'tiny'])
+    };
+
+    const aTagClassNames = classNames('vp-type-link', {
+        [`--${size}`]: size
+    });
+
+    return <a {...{ href, className: aTagClassNames }}>{children}</a>;
+};
+
+export default Link;
