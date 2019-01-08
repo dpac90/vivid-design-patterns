ec6834d679451d5b6289cd692bd3fe82499bc207
Jeff Borders <jeff.borders@vividseats.com>
2019-01-08 14:53:45 -0600

    WEB-5692 finishing link docs

--- src/components/atoms/Link.jsx
+++ src/components/atoms/Link.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-const Link = ({ children, className, href, size }) => {
+const Link = ({ children, className, href }) => {
     Link.propTypes = {
         children: PropTypes.node,
         className: PropTypes.string,
-        href: PropTypes.string.isRequired,
-        size: PropTypes.oneOf(['body-1', 'body-2', 'small', 'tiny'])
+        href: PropTypes.string.isRequired
     };
 
     const aTagClassNames = classNames('vp-type-link', {
-        [`--${size}`]: !!size,
         [className]: !!className
     });
 
