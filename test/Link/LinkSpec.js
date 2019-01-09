23fb6c07742a65a2e473ae2f029c2e27e327c2c5
Jeff Borders <jeff.borders@vividseats.com>
2019-01-09 15:53:15 -0600

    WEB-5692 removing comments from Link tests

--- test/Link/LinkSpec.js
+++ test/Link/LinkSpec.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import Link from '../../src/components/atoms/Link';
 
-describe('<Button />', () => {
+describe('<Link />', () => {
     const fakeHref = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
 
     it('renders an <a> tag', () => {
@@ -21,105 +21,4 @@ describe('<Button />', () => {
         wrapper.find('.vp-type-link').simulate('click');
         expect(mockOnClick).toHaveBeenCalledTimes(1);
     });
-
-    // it('renders a large button', () => {
-    //     const wrapper = shallow(<Button size="large" />);
-    //     expect(wrapper.find('.vp-button').hasClass('--large')).toBe(true);
-    // });
-    //
-    // it('renders a primary button', () => {
-    //     const wrapper = shallow(<Button />);
-    //     expect(wrapper.find('.vp-button').hasClass('--secondary', '--tertiary')).toBe(false);
-    // });
-    //
-    // it('renders a secondary button', () => {
-    //     const wrapper = shallow(<Button importance="secondary" />);
-    //     expect(wrapper.find('.vp-button').hasClass('--secondary')).toBe(true);
-    // });
-    //
-    // it('renders a tertiary button', () => {
-    //     const wrapper = shallow(<Button importance="tertiary" />);
-    //     expect(wrapper.find('.vp-button').hasClass('--tertiary')).toBe(true);
-    // });
-    //
-    // it('renders a raised button', () => {
-    //     const wrapper = shallow(<Button raised />);
-    //     expect(wrapper.find('.vp-button').hasClass('--raised')).toBe(true);
-    // });
-    //
-    // it('renders grouped buttons', () => {
-    //     const wrapper = mount(
-    //         <React.Fragment>
-    //             <Button grouped>Grouped Button</Button>
-    //             <Button grouped>Grouped Button</Button>
-    //         </React.Fragment>
-    //     );
-    //     expect(wrapper.find('.vp-grouped-button').length).toBe(2);
-    // });
-    //
-    // it('renders grouped buttons with selected state for the  button element', () => {
-    //     const wrapper = mount(
-    //         <React.Fragment>
-    //             <Button grouped>Grouped Button</Button>
-    //             <Button grouped>Grouped Button</Button>
-    //             <Button grouped selected>
-    //                 Grouped Button
-    //             </Button>
-    //         </React.Fragment>
-    //     );
-    //     expect(wrapper.find('.vp-grouped-button').length).toBe(3);
-    //     expect(
-    //         wrapper
-    //             .find('.vp-grouped-button')
-    //             .last()
-    //             .hasClass('--selected')
-    //     ).toBe(true);
-    // });
-    //
-    // it('handles onClick event', () => {
-    //     const mockOnClick = jest.fn();
-    //     const wrapper = shallow(<Button size="small" onClick={mockOnClick} />);
-    //     wrapper.find('.vp-button').simulate('click');
-    //     expect(mockOnClick).toHaveBeenCalledTimes(1);
-    // });
-    //
-    // it('prevents onClick when button is disabled', () => {
-    //     const mockOnClick = jest.fn();
-    //     const wrapper = mount(<Button size="small" disabled onClick={mockOnClick} />);
-    //     wrapper.find('.vp-button').simulate('click');
-    //     expect(mockOnClick).toHaveBeenCalledTimes(0);
-    // });
-    //
-    // it('handles onMouseOver event', () => {
-    //     const mockOnClick = jest.fn();
-    //     const wrapper = mount(<Button size="small" onMouseOver={mockOnClick} />);
-    //     wrapper.find('.vp-button').simulate('mouseover');
-    //     expect(mockOnClick).toHaveBeenCalledTimes(1);
-    // });
-    //
-    // it('handles onMouseOut event', () => {
-    //     const mockOnClick = jest.fn();
-    //     const wrapper = mount(<Button size="small" onMouseOut={mockOnClick} />);
-    //     wrapper.find('.vp-button').simulate('mouseout');
-    //     expect(mockOnClick).toHaveBeenCalledTimes(1);
-    // });
-    //
-    // it('handles onBlur event', () => {
-    //     const mockOnClick = jest.fn();
-    //     const wrapper = mount(<Button size="small" onBlur={mockOnClick} />);
-    //     wrapper.find('.vp-button').simulate('blur');
-    //     expect(mockOnClick).toHaveBeenCalledTimes(1);
-    // });
-    //
-    // it('handles onFocus event', () => {
-    //     const mockOnClick = jest.fn();
-    //     const wrapper = mount(<Button size="small" onFocus={mockOnClick} />);
-    //     wrapper.find('.vp-button').simulate('focus');
-    //     expect(mockOnClick).toHaveBeenCalledTimes(1);
-    // });
-    //
-    // it('renders an anchor tag when an href is provided', () => {
-    //     const wrapper = shallow(<Button href="www.vividseats.com" />);
-    //     expect(wrapper.find('a').hasClass('vp-button')).toBe(true);
-    // });
 });
