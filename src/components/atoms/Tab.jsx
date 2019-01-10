346638ff7c047eaea02bd2fdd09d273986f8f181
Jeff Borders <jeff.borders@vividseats.com>
2019-01-10 14:48:03 -0600

    WEB-5741 adding mouseenter and mouseleave callbacks to tabs

--- src/components/atoms/Tab.jsx
+++ src/components/atoms/Tab.jsx
@@ -2,18 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-const Tab = ({ children, className, href, active, onClick = () => {}, ...attributes }) => {
+const Tab = ({
+    children,
+    className,
+    href,
+    active,
+    onClick = () => {},
+    onMouseEnter = () => {},
+    onMouseLeave = () => {},
+    ...attributes
+}) => {
     Tab.propTypes = {
         children: PropTypes.node,
         className: PropTypes.string,
         href: PropTypes.string,
         active: PropTypes.bool,
-        onClick: PropTypes.func
+        onClick: PropTypes.func,
+        onMouseEnter: PropTypes.func,
+        onMouseLeave: PropTypes.func
     };
 
     const props = {
         href,
         onClick,
+        onMouseEnter,
+        onMouseLeave,
         className: classNames('vp-tab', {
             [className]: !!className
         }),
