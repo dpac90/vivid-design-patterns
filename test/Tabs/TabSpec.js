539d58a67e49e5ff5b3631a021988263730d067a
Jeff Borders <jeff.borders@vividseats.com>
2019-01-10 15:11:17 -0600

    WEB-5741 finishing tab and tabgroup unit tests

--- test/Tabs/TabSpec.js
+++ test/Tabs/TabSpec.js
@@ -7,23 +7,33 @@ describe('<Tab />', () => {
     const fakeHref = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
 
     it('renders an <a> tag', () => {
-        const wrapper = shallow(
-            <LinkGroup>
-                <Link href={fakeHref} />
-            </LinkGroup>
-        );
-        expect(wrapper.find('a.vp-type-link').exists()).toBe(true);
+        const wrapper = shallow(<Tab href={fakeHref} />);
+        expect(wrapper.find('a.vp-tab').exists()).toBe(true);
     });
 
     it('renders an <a> tag with custom attributes', () => {
-        const wrapper = shallow(<Link href={fakeHref} id="myLink" />);
-        expect(wrapper.find('#myLink').hasClass('vp-type-link')).toBe(true);
+        const wrapper = shallow(<Tab href={fakeHref} id="myTab" />);
+        expect(wrapper.find('#myTab').hasClass('vp-tab')).toBe(true);
     });
 
     it('handles onClick event', () => {
         const mockOnClick = jest.fn();
-        const wrapper = shallow(<Link href={fakeHref} onClick={mockOnClick} />);
-        wrapper.find('.vp-type-link').simulate('click');
+        const wrapper = shallow(<Tab href={fakeHref} onClick={mockOnClick} />);
+        wrapper.find('.vp-tab').simulate('click');
         expect(mockOnClick).toHaveBeenCalledTimes(1);
+    });
+
+    it('handles onMouseEnter event', () => {
+        const mockOnMouseEnter = jest.fn();
+        const wrapper = shallow(<Tab href={fakeHref} onMouseEnter={mockOnMouseEnter} />);
+        wrapper.find('.vp-tab').simulate('mouseenter');
+        expect(mockOnMouseEnter).toHaveBeenCalledTimes(1);
+    });
+
+    it('handles onMouseLeave event', () => {
+        const mockOnMouseLeave = jest.fn();
+        const wrapper = shallow(<Tab href={fakeHref} onMouseLeave={mockOnMouseLeave} />);
+        wrapper.find('.vp-tab').simulate('mouseleave');
+        expect(mockOnMouseLeave).toHaveBeenCalledTimes(1);
     });
 });
