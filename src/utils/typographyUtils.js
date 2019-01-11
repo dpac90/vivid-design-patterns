801970ec9308175d5e75060d539270d4685d5e3d
Yucheng Lin <yuchenglin3@gmail.com>
2019-01-11 15:12:59 -0600

    fix bld style modifier

--- src/utils/typographyUtils.js
+++ src/utils/typographyUtils.js
@@ -7,11 +7,20 @@ import React from 'react';
 /* eslint react/jsx-filename-extension: 0 */
 /* eslint no-underscore-dangle: 0 */
 
+class __dummyPropTypesComponent extends React.Component {
+    render() {
+        console.warn(
+            'Warning: this is a dummy component used to generated proptypes for React Docgen and DOCZ. Do not use this component in production'
+        );
+        return React.createElement('div', null, `Hello`);
+    }
+}
+
 function getTypeClassNames(baseClassName, { weight, height, state, alignment, capitalization, truncate, list, className }) {
     return classNames(baseClassName, {
         [className]: className,
         '--blk': weight === 'blk' || weight === 'black',
-        '--bold': weight === 'bld' || weight === 'bold',
+        '--bld': weight === 'bld' || weight === 'bold',
         '--med': weight === 'med' || weight === 'medium',
         '--truncate': truncate,
         '--list': list,
@@ -40,13 +49,6 @@ const TYPOGRAPHY_PROP_TYPES = {
     /**  Add padding for list items. --list */
     list: PropTypes.bool
 };
-
-function __dummyPropTypesComponent() {
-    console.warn(
-        'Warning: this is a dummy component used to generated proptypes for React Docgen and DOCZ. Do not use this component in production'
-    );
-    return <div>sadasd</div>;
-}
 
 __dummyPropTypesComponent.propTypes = TYPOGRAPHY_PROP_TYPES;
 
