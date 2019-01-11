a74443c53e93ae26c814062219360336a55d7a90
Yucheng Lin <yuchenglin3@gmail.com>
2019-01-11 10:28:01 -0600

    add examples for modifiers in docs

--- src/utils/typographyUtils.js
+++ src/utils/typographyUtils.js
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
+import React from 'react';
 
 function getTypeClassNames(baseClassName, { weight, height, state, alignment, capitalization, truncate, list, className }) {
     return classNames(baseClassName, {
@@ -19,13 +20,29 @@ function getTypeClassNames(baseClassName, { weight, height, state, alignment, ca
 const TYPOGRAPHY_PROP_TYPES = {
     className: PropTypes.string,
     children: PropTypes.node,
+    /** Font weight. --blk, --bld, ---med */
     weight: PropTypes.oneOf(['black', 'bold', 'medium']),
+    /** Line height. --compressed, --expanded */
     height: PropTypes.oneOf(['compressed', 'expanded']),
+    /** --disabled, --inverted, --muted */
     state: PropTypes.oneOf(['disabled, inverted, muted']),
+    /** Text alignment  --left, --center, --right */
     alignment: PropTypes.oneOf(['left', 'center', 'right']),
+    /** ---uppercase, --lowercase **/
     capitalization: PropTypes.oneOf(['uppercase', 'lowercase']),
+    /** Truncate text with ellipsis. --truncate */
     truncate: PropTypes.bool,
+    /**  Add padding for list items. --list */
     list: PropTypes.bool
 };
 
-export { getTypeClassNames, TYPOGRAPHY_PROP_TYPES };
+function __dummyPropTypesComponent() {
+    console.warn(
+        'Warning: this is a dummy component used to generated proptypes for React Docgen and DOCZ. Do not use this component in production'
+    );
+    return <div>sadasd</div>;
+}
+
+__dummyPropTypesComponent.propTypes = TYPOGRAPHY_PROP_TYPES;
+
+export { getTypeClassNames, TYPOGRAPHY_PROP_TYPES, __dummyPropTypesComponent };
