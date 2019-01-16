8e42441ee5328a660a79dc195342dd6305ebda34
Paige Vogenthaler <paige.vogenthaler@vividseats.com>
2019-01-16 11:18:04 -0600

    WEB-5696 remove unused import

--- src/components/molecules/Modal.jsx
+++ src/components/molecules/Modal.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 
 import ModalHeader from '../atoms/ModalHeader';
 import ModalBody from '../atoms/ModalBody';
