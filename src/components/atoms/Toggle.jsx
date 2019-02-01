e1801ec0bd32b09404b76181fedba24306de5dee
Jeff Borders <jeff.borders@vividseats.com>
2019-02-01 16:53:54 -0600

    WEB-5736 adding toggle

--- src/components/atoms/Toggle.jsx
+++ src/components/atoms/Toggle.jsx
@@ -1,38 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-function Toggle({ title, className, ariaLabel, on, onToggle: onToggleProp, ...htmlProps }) {
-    const onToggle = e => {
-        onToggleProp(e.target.checked);
+class Toggle extends Component {
+    static propTypes = {
+        title: PropTypes.string,
+        className: PropTypes.string,
+        ariaLabel: PropTypes.string,
+        on: PropTypes.bool,
+        onToggle: PropTypes.func.isRequired
     };
 
-    return (
-        <div className={`toggle ${className}`} {...htmlProps}>
-            {!!title && <h4 className="toggle-title">{title}</h4>}
-            <span>
-                <label className="toggle-switch" aria-label={ariaLabel}>
-                    <input type="checkbox" onChange={onToggle} checked={on} />
-                    <span className="toggle-button round" />
-                </label>
-            </span>
-        </div>
-    );
+    static defaultProps = {
+        title: '',
+        className: '',
+        ariaLabel: 'toggle',
+        onToggle: () => {}
+    };
+
+    state = {
+        on: this.props.on
+    };
+
+    onToggle = e => {
+        const on = e.target.checked;
+
+        if (!this.isControlled('on')) {
+            this.setState({ on }, () => {
+                this.props.onToggle(this.state.on);
+            });
+        }
+
+        this.props.onToggle(on);
+    };
+
+    isControlled = propName => {
+        return this.props[propName] !== undefined;
+    };
+
+    render() {
+        const { title, className, ariaLabel, on, onToggle, ...htmlProps } = this.props;
+        const checked = this.isControlled('on') ? this.props.on : this.state.on;
+
+        return (
+            <div className={`toggle ${className}`} {...htmlProps}>
+                {!!title && <h4 className="toggle-title">{title}</h4>}
+                <span>
+                    <label className="toggle-switch" aria-label={ariaLabel}>
+                        <input type="checkbox" onChange={this.onToggle} checked={on} />
+                        <span className="toggle-button round" />
+                    </label>
+                </span>
+            </div>
+        );
+    }
 }
-
-Toggle.propTypes = {
-    title: PropTypes.string,
-    className: PropTypes.string,
-    ariaLabel: PropTypes.string,
-    on: PropTypes.bool,
-    onToggle: PropTypes.func.isRequired
-};
-
-Toggle.defaultProps = {
-    title: '',
-    className: '',
-    ariaLabel: 'toggle',
-    on: false,
-    onToggle: () => {}
-};
 
 export default Toggle;
