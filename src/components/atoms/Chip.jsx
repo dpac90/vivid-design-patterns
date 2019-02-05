87ccaf01b0a27435a51ad32a16b8a9c40a721f04
Jeff Borders <jeff.borders@vividseats.com>
2019-02-05 16:22:07 -0600

    WEB-5740 cleanup

--- src/components/atoms/Chip.jsx
+++ src/components/atoms/Chip.jsx
@@ -1,37 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-function Chip({ label, onClose, value, className, ...htmlAttributes }) {
-    const onCloseVal = value || label;
-    const classNameList = classNames('vp-chip', {
-        [className]: !!className
-    });
+class Chip extends Component {
+    static propTypes = {
+        className: PropTypes.string,
+        label: PropTypes.string.isRequired,
+        value: PropTypes.string,
+        onClose: PropTypes.func
+    };
 
-    return (
-        <span
-            className={classNameList}
-            onClick={() => {
-                onClose(onCloseVal);
-            }}
-            {...htmlAttributes}>
-            {label}
-        </span>
-    );
+    static defaultProps = {
+        className: '',
+        value: '',
+        onClose: () => {}
+    };
+
+    onClose = e => {
+        e.preventDefault();
+        const { value, label } = this.props;
+        this.props.onClose(value || label);
+    };
+
+    onKeyPress = e => {
+        e.preventDefault();
+        if (e.keyCode === 13) {
+            this.onClose();
+        }
+    };
+
+    render() {
+        const { onKeyPress, onClose, props } = this;
+        const { label, value, onClose: callback, className: classes, ...htmlAttributes } = props;
+        const className = classNames('vp-chip', {
+            [classes]: !!classes
+        });
+
+        return (
+            <a href="#!" onClick={onClose} onKeyPress={onKeyPress} {...{ className }} {...htmlAttributes}>
+                {label}
+            </a>
+        );
+    }
 }
-
-Chip.propTypes = {
-    className: PropTypes.string,
-    label: PropTypes.string.isRequired,
-    value: PropTypes.string,
-    onClose: PropTypes.func
-};
-
-Chip.defaultProps = {
-    className: '',
-    label: '',
-    value: '',
-    onClose: () => {}
-};
 
 export default Chip;
