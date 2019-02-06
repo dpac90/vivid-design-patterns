6a11d9419961c0b57c2a751eff842e14d147b8f6
Jeff Borders <jeff.borders@vividseats.com>
2019-02-06 13:26:21 -0600

    WEB-5736: adding toggle component (#32)
    * WEB-5736 initial commit
    
    * WEB-5736 adding toggle
    
    * WEB-5736 adding use cases and cleaning up
    
    * WEB-5736 changing eslint rules and accommodating linting errors
    
    * v0.0.0-WEB-5736-1549298814
    
    * WEB-5736 adding title
    
    * add unit tests
    
    * WEB-5736 removing redundant boolean
    
    * WEB-5736 adding toggle title specific styles
    
    * WEB-5736 pr changes
    
    * WEB-5736 unit test changes
    
    * v0.0.0-WEB-5736-1549310272
    
    * WEB-5736 toggle pattern styles
    
    * WEB-5736 updating tests
    
    * v0.0.0-WEB-5736-1549384884
    
    * WEB-5736 new styles
    
    * v0.0.0-WEB-5736-1549393123
    
    * updating minor version prepublish

---
+++ src/components/atoms/Toggle.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Toggle extends Component {
+    static propTypes = {
+        title: PropTypes.string,
+        className: PropTypes.string,
+        ariaLabel: PropTypes.string,
+        on: PropTypes.bool,
+        onToggle: PropTypes.func,
+        defaultOn: PropTypes.bool
+    };
+
+    static defaultProps = {
+        title: '',
+        className: '',
+        ariaLabel: 'toggle',
+        onToggle: () => {},
+        defaultOn: false
+    };
+
+    state = {
+        on: this.props.on || this.props.defaultOn
+    };
+
+    onToggle = e => {
+        const on = e.target.checked;
+
+        if (!this.isControlled('on')) {
+            this.setState({ on }, () => {
+                this.props.onToggle(this.state.on);
+            });
+        } else {
+            this.props.onToggle(on);
+        }
+    };
+
+    isControlled = propName => this.props[propName] !== undefined;
+
+    render() {
+        const { title, className, ariaLabel, on, onToggle, defaultOn, ...htmlProps } = this.props;
+        const checked = this.isControlled('on') ? this.props.on : this.state.on;
+
+        return (
+            <label className={`vp-toggle ${className}`} {...htmlProps}>
+                {!!title && <span className="vp-toggle__label">{title}</span>}
+                <input className="vp-toggle__control" type="checkbox" onChange={this.onToggle} checked={checked} hidden />
+                <span className="vp-toggle__thumb" />
+            </label>
+        );
+    }
+}
+
+export default Toggle;
---
+++ test/Toggle/ToggleSpec.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import Toggle from '../../src/components/atoms/Toggle';
+
+describe('<Toggle />', () => {
+    const toggleTitle = 'Toggle';
+    const ToggleComponent = <Toggle title={toggleTitle} />;
+
+    it('Renders an label and checkbox input', () => {
+        const wrapper = shallow(ToggleComponent);
+        expect(wrapper.find('input').exists()).toBe(true);
+        expect(wrapper.find('label').exists()).toBe(true);
+    });
+
+    it('renders a label when provided', () => {
+        const wrapper = mount(ToggleComponent);
+        expect(wrapper.find('.vp-toggle__label').text()).toBe(toggleTitle);
+    });
+
+    describe('When <Toggle/> is used as an uncontrolled component', () => {
+        it('it "toggles" on when clicked', () => {
+            const wrapper = mount(ToggleComponent);
+            wrapper.find('input').simulate('change', { target: { checked: true } });
+            expect(wrapper.state('on')).toBe(true);
+            expect(wrapper.find('input').prop('checked')).toBeTruthy();
+        });
+
+        it('it renders with a default value', () => {
+            const wrapper = mount(<Toggle defaultOn={true} />);
+            expect(wrapper.state('on')).toBe(true);
+            expect(wrapper.find('input').prop('checked')).toBeTruthy();
+            wrapper.find('input').simulate('change', { target: { checked: false } });
+            expect(wrapper.state('on')).toBe(false);
+            expect(wrapper.find('input').prop('checked')).toBeFalsy();
+        });
+    });
+
+    describe('When <Toggle/> is used as an controlled component', () => {
+        const mockOnToggle = jest.fn();
+        const controlledToggleComponent = <Toggle on onToggle={mockOnToggle} />;
+        it('it triggers an onChange with correct values when called when toggled', () => {
+            const wrapper = mount(controlledToggleComponent);
+            wrapper.find('input').simulate('change', { target: { checked: false } });
+            expect(mockOnToggle).toHaveBeenCalledWith(false);
+            wrapper.find('input').simulate('change', { target: { checked: true } });
+            expect(mockOnToggle).toHaveBeenCalledWith(true);
+        });
+
+        it('it "toggles" on when new on prop is passed in', () => {
+            const wrapper = mount(<Toggle on />);
+            expect(wrapper.find('input').prop('checked')).toBeTruthy();
+            wrapper.setProps({ on: false });
+            expect(wrapper.find('input').prop('checked')).toBeFalsy();
+        });
+    });
+});
