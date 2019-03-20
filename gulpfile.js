bfc4187e73187cc89cd0b41b007ea5da2aadf33e
Jeff Borders <jeff.borders@vividseats.com>
2019-03-20 10:07:57 -0500

    Web 6070 (#60)
    * WEB-6070 initial optimization
    
    * WEB-6070 updating version
    
    * WEB-6070 updating dependency
    
    * WEB-6070 publishing
    
    * WEB-6070 troubleshooting
    
    * WEB-6070 reverting
    
    * WEB-6070 updating dependency block
    
    * WEB-6070 updating version after publish
    
    * WEB-6070 update version

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,23 @@
 const path = require('path');
 const gulp = require('gulp');
 const rollUpEach = require('gulp-rollup-each');
+const changed = require('gulp-changed');
+const gulpif = require('gulp-if');
 const rollUpConfig = require('./rollup.config');
+const isDev = process.env.NODE_ENV === 'dev';
 
 gulp.task('bundle_js', () => {
+    const dist = 'dist';
+
     return gulp
         .src('src/components/**/*.{js,jsx}')
+        .pipe(gulpif(isDev, changed(dist)))
         .pipe(
-            rollUpEach(rollUpConfig, file => {
-                return {
-                    format: 'cjs',
-                    name: path.basename(file.path, file.extname),
-                    globals: {
-                        react: 'React',
-                        'prop-types': 'PropTypes'
-                    }
-                };
-            })
+            rollUpEach(rollUpConfig, file => ({
+                format: 'cjs'
+            }))
         )
-        .pipe(gulp.dest('dist/'));
+        .pipe(gulp.dest(dist));
 });
 
 gulp.task('watch_js', () => {
--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,16 @@
 const resolve = require('rollup-plugin-node-resolve');
 const babel = require('rollup-plugin-babel');
 const uglify = require('rollup-plugin-terser').terser;
+const replace = require('rollup-plugin-replace');
 const commonjs = require('rollup-plugin-commonjs');
-const isDev = process.env.NODE_ENV === 'dev';
+
+const nodeEnv = JSON.stringify(process.env.NODE_ENV || 'production');
+const isDev = nodeEnv === 'dev';
+const packageJson = require('./package.json');
+const external = Object.keys(packageJson.peerDependencies);
 
 module.exports = {
-    external: ['prop-types', 'react', 'react-dom'],
+    external,
     plugins: [
         resolve({
             browser: true,
@@ -14,6 +19,9 @@ module.exports = {
         }),
         babel({
             exclude: 'node_modules/**'
+        }),
+        replace({
+            'process.env.NODE_ENV': nodeEnv
         }),
         commonjs({
             extensions: ['.js', '.jsx']
